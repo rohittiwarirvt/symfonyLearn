dea20b2786e2334b481277a2f1c60b8d2d4d5e71
Rohit Tiwari <rohittiwarirvt@gmail.com>
2017-10-23 12:11:45 +0530

    Added some files for testing purpose

--- src/Acme/TestBundle/Entity/User.php
+++ src/Acme/TestBundle/Entity/User.php
@@ -9,20 +9,18 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
  */
-
 class User extends BaseUser
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
-  /**
-   * @ORM\Id
-   * @ORM\Column(type="integer")
-   * @ORM\GeneratedValue(strategy="AUTO")
-   */
-
-  protected $id;
-
-  public function __construct()
-  {
-    parent::__construct();
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
